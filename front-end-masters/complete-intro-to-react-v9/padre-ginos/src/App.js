efed809d7b57ea87c55cf380d83cf56af70f40b2
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-01-17 21:05:09 -0500

    citr-v9: add prettier and npm project with gitignore

--- front-end-masters/complete-intro-to-react-v9/padre-ginos/src/App.js
+++ front-end-masters/complete-intro-to-react-v9/padre-ginos/src/App.js
@@ -3,8 +3,7 @@ const Pizza = (props) => {
     React.createElement("h1", {}, props.name),
     React.createElement("p", {}, props.description),
   ]);
-}
-
+};
 
 const HardPizza = () => {
   // Array to return multiple top-level (sibling components)
@@ -12,31 +11,27 @@ const HardPizza = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "The Pepperoni Pizza"),
     React.createElement("p", {}, "Mozzarella Cheese"),
-  ])
-}
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Padre Ginos"),
-      // {} passes properties to pizza, it's empty since there's no values to pass
-      React.createElement(HardPizza, {}, []),
-      React.createElement(HardPizza),
-      React.createElement(Pizza, {
-        name: 'The Reign Supreme',
-        description: 'The Monarch of Supreme Pizza'
-      }),
-      React.createElement(Pizza, {
-        name: 'The Grass Is Greener',
-        description: 'Veggie Lovers Paradise'
-      }),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Ginos"),
+    // {} passes properties to pizza, it's empty since there's no values to pass
+    React.createElement(HardPizza, {}, []),
+    React.createElement(HardPizza),
+    React.createElement(Pizza, {
+      name: "The Reign Supreme",
+      description: "The Monarch of Supreme Pizza",
+    }),
+    React.createElement(Pizza, {
+      name: "The Grass Is Greener",
+      description: "Veggie Lovers Paradise",
+    }),
+  ]);
+};
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 const appEl = React.createElement(App);
 root.render(appEl);
