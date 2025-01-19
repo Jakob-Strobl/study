efc7b5a32d6f4838b0baa75492cfeb1f52030b0b
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-01-18 20:23:45 -0500

    citr-v9: add eslint-plugin-react and other react rules

--- front-end-masters/complete-intro-to-react-v9/padre-ginos/eslint.config.mjs
+++ front-end-masters/complete-intro-to-react-v9/padre-ginos/eslint.config.mjs
@@ -1,11 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 // It didn't work at first, but it does work on helix!!!
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
@@ -18,6 +28,10 @@ export default [
         ecmaFeatures: {
           jsx: true,
         },
+      },
+      rules: {
+        "react/no-unescaped-entities": "off", // You can't write single quotes lol ' -> &apos
+        "react/prop-types": "off",
       },
     },
   },
