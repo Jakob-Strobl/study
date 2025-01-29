bcfa1130d3609b449869fbd8146cc96a6e3030b6
Jakob-Strobl <jakobpstrobl@gmail.com>
2025-01-28 23:20:36 -0500

    citr-v9: past orders fetch

--- front-end-masters/complete-intro-to-react-v9/padre-ginos/src/routes/past.lazy.jsx
+++ front-end-masters/complete-intro-to-react-v9/padre-ginos/src/routes/past.lazy.jsx
@@ -1,5 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import getPastOrders from "../api/getPastOrders";
 
 export const Route = createLazyFileRoute("/past")({
-  component: () => <p>TEST</p>,
+  component: PastOrderRoute,
 });
+
+function PastOrderRoute() {
+  const [page, setPage] = useState(1);
+  const { isLoading, data } = useQuery({
+    enabled: false,
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    cacheTime: 0,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="past-orders">
+        <h2>Loading</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((order) => {
+            return (
+              <tr key={order.order_id}>
+                <td>{order.order_id}</td>
+                <td>{order.date}</td>
+                <td>{order.time}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
